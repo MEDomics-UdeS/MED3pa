07f2b7c4bbd2cd5eecde8e9ba19470e9774cc29d
lyna1404 <jl_chikouche@esi.dz>
2024-07-04 11:09:49 -0400

    updated the ci workflows

--- docs/conf.py
+++ docs/conf.py
@@ -22,7 +22,7 @@ copyright = '2024, MEDomics Consortium'
 author = 'Lyna Chikouche, Ludmila Amriou, Olivier Lefebvre, Martin Vallières'
 
 # The full version, including alpha/beta/rc tags
-release = '0.0.1'
+release = '0.1.0'
 
 
 # -- General configuration ---------------------------------------------------
