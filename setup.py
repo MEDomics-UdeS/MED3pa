51a5924b8fc65416cd704cbf7e007d4c8cefa418
lyna1404 <jl_chikouche@esi.dz>
2024-07-22 14:15:27 -0400

    added option to use same models for test set

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open('requirements.txt') as f:
 
 setup(
     name="MED3pa",
-    version="0.1.11",
+    version="0.1.12",
     author="MEDomics consortium",
     author_email="medomics.info@gmail.com",
     description="Python Open-source package for ensuring robust and reliable ML models deployments",
