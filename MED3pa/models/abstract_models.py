e181c6bf4b8961d97ddff3a43e6dd9902d700d7a
Olivier998 <olivier.lefebvre3@usherbrooke.ca>
2024-11-29 12:08:56 -0500

    Changed matplotlib background

--- MED3pa/models/abstract_models.py
+++ MED3pa/models/abstract_models.py
@@ -13,6 +13,7 @@ import pickle
 import json
 
 import matplotlib.pyplot as plt
+matplotlib.use('Agg')  # Use a non-interactive backend
 import numpy as np
 import pandas as pd
 from sklearn.base import BaseEstimator, ClassifierMixin
