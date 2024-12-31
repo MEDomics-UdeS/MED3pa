41ec467c2a920313d92c6d7ebdb298fab3124dd5
Olivier998 <olivier.lefebvre3@usherbrooke.ca>
2024-12-31 14:53:18 -0500

    wait few seconds before sending new tasks

--- MED3pa/ray_checkpointing/utils.py
+++ MED3pa/ray_checkpointing/utils.py
@@ -8,6 +8,7 @@ def wait_for_pending_tasks(threshold=600, check_interval=10):
     It prevents sending too many tasks at the same time on the cluster. If too many tasks are pending,
     wait for `check_interval` seconds before evaluating the number of pending tasks.
     """
+    time.sleep(check_interval)  # Initial sleep to ensure enough time between each new tasks
     while True:
         pending_count = len(list_tasks(filters=[('state', '=', 'PENDING_NODE_ASSIGNMENT')],
                                        raise_on_missing_output=False, limit=800))
