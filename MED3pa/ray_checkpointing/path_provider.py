ff8f121c725bb703cb7aa94eabe9354f503cc0fb
Olivier998 <olivier.lefebvre3@usherbrooke.ca>
2025-01-01 20:48:58 -0500

    test

--- MED3pa/ray_checkpointing/path_provider.py
+++ MED3pa/ray_checkpointing/path_provider.py
@@ -9,8 +9,11 @@ class PathProvider:
 
     def __call__(self, *args, **kwargs):
         print(f'fn_hash: {self.fn_hash} & {args=} & {kwargs=}')
+        kwargs2 = kwargs.copy()
+        kwargs2.pop('ray_tqdm_bar')
         print(f'fn_hashed: {hashing.hash((self.fn_hash), "blake2b")[: 32]} & '
-              f'args {hashing.hash((args), "blake2b")[: 32]} &'
-              f'kwargs {hashing.hash((kwargs), "blake2b")[: 32]}')
+              f'args {hashing.hash((args), "blake2b")[: 32]} & '
+              f'kwargs {hashing.hash((kwargs), "blake2b")[: 32]} &'
+              f'kwargs no tqdm {hashing.hash((kwargs2), "blake2b")[: 32]}')
         call_hash = hashing.hash((self.fn_hash, args, kwargs), "blake2b")[: 32]
         return f"{self.fn_id}/{call_hash}"
