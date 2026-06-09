b85934a37bf1e551785ca3cc3547949b105cc3cd
Olivier998 <olivier.lefebvre3@usherbrooke.ca>
2026-06-09 14:02:15 -0400

    release 1.0.4

--- docs/conf.py
+++ docs/conf.py
@@ -23,7 +23,7 @@ copyright = '2025, MEDomics Consortium'
 author = 'Olivier Lefebvre, Lyna Chikouche, Ludmila Amriou, Martin Vallières'
 
 # The full version, including alpha/beta/rc tags
-release = '1.0.3'
+release = '1.0.4'
 
 
 # -- General configuration ---------------------------------------------------
